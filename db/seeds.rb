70e639145dbf0fca2a841bc3ba7da8e6b4a27149
¨kericho¨ <lvisrono90@gmail.com>
2023-03-02 09:19:30 +0300

    add a seed file

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+User.destroy_all
+Task.destroy_all
+puts "🌱 Seeding spices..."
+User.create(username: 'tosh', email: 'tosh@gmail.com', password: '1111')
+User.create(username: 'kev', email: 'kev@gmail.com', password: '2222')
+User.create(username: 'dan', email: 'dan@gmail.com', password: '3333')
+User.create(username: 'kim', email: 'kim@gmail.com', password: '4444')
+user1 = User.create(username: 'tess', email: 'tess@gmail.com', password: '1234')
+
+
+
+Task.create(category: "work", todos: "finish up the project",  user_id: "user1.id")
+
+
+
+
+
+puts "✅ Done seeding!"
