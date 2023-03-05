a5f85119db9b3202624a0b45677e98e40e09829a
¨kericho¨ <lvisrono90@gmail.com>
2023-03-05 17:39:40 +0300

    create a class task in model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,16 @@
 class Task < ActiveRecord::Base
     belongs_to :user
+
+    def self.create_new_task_with_defaults(hash)
+        name = hash["name"] ? hash["name"] : "create Task"
+        status = hash["status"] ? hash["completed"] : "Not Completed"
+        completed = hash["completed"] ? hash["completed"] : false
+        self.new(
+            name: name,
+            due_date: hash["due_date"],
+            description: hash["description"],
+            status: status,
+            completed: completed
+        )
+    end
 end
