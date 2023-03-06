8bbf2a98bb18c8ca2bdc0293faf267fab27d50bb
¨kericho¨ <lvisrono90@gmail.com>
2023-03-06 07:26:15 +0300

    rewrite model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,14 @@ class User < ActiveRecord::Base
     validates :username, :presence => true, :uniqueness => true
     validates :password, :presence => true
     has_many :tasks
+
+    def self.create_new_user(hash)
+        name = hash["name"] ? hash["name"] : "register user"
+        self.new(
+            name: name,
+            email: hash["email"],
+            password: hash["password"]
+        )
+    end
 end
 
